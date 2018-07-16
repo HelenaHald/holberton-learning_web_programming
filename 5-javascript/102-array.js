8255d015ad411dc42d2acf55cf213f6b7ff6a95d
Helena Hald <helenahald@gmail.com>
2018-07-16 15:31:26 -0700

    wrbwrbrynyef

---
+++ 5-javascript/102-array.js
@@ -0,0 +1,1 @@
+var dwarf=["Doc","Happy","Sleepy","Grumpy","Dopey","Bashful","Sneezy"]
---
+++ 5-javascript/my_first_javascript_variable.js
@@ -0,0 +1,5 @@
+let variable_test=("I love Javascript!")
+undefined
+console.log(variable_test)
+VM514:1 I love Javascript!
+undefined
